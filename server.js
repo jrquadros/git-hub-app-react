fc56a0eb77c2366e05e7b66690dd50ca6a68fa43
jrquadros <quadrosjunioradits@gmail.com>
2019-02-08 00:45:30 -0300

    server added

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const webpack = require('webpack')
+const WebpackDevServer = require('webpack-dev-server')
+const config = require('./webpack.config')
+
+new WebpackDevServer(webpack(config),{
+    hot:true,
+    historyApiFallback: true,
+    stats:{ colors: true }
+}).listen(3000, (err)=>{
+    if(err){
+        return console.log(err)
+    }
+
+    console.log('Listening on http://localhost:3000')
+})
