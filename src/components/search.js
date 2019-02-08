7777ba168950546e21280471aa12430293abdd5f
jrquadros <quadrosjunioradits@gmail.com>
2019-02-08 19:45:34 -0300

    search component added

---
+++ src/components/search.js
@@ -0,0 +1,11 @@
+'use strict'
+
+import React from 'react'
+
+const Search = () => (
+  <div className='search'>
+    <input type='search' placeholder='Digite o nome do usuário' />
+  </div>
+)
+ 
+export default Search
