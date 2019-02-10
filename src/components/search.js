721818fa4ba9bb225598ed8d4b42f75d2ab6963c
jrquadros <quadrosjunioradits@gmail.com>
2019-02-10 19:09:27 -0300

    Request of user data

--- src/components/search.js
+++ src/components/search.js
@@ -1,12 +1,41 @@
 'use strict'
 
 import React from 'react'
+import Fetch from 'node-fetch'
+// import Ajax from '@fdaciuk/ajax'
 
 const Search = () => (
   <div className='search'>
     <input
       type='search'
-      placeholder='Digite o nome do usuário' />
+      placeholder='Digite o nome do usuário'
+      onKeyUp={(e) => {
+        const value = e.target.value
+        const keyCode = e.which || e.keyCode
+        const ENTER = 13
+
+        if (keyCode === ENTER) {
+          Fetch(`https://api.github.com/users/${value}`, {
+            method: 'get'
+          })
+            .then((response) => {
+              response.json()
+                .then((result) => {
+                  console.log(result)
+                  console.log(result.login)
+                })
+            })
+        }
+
+        /*  if (keyCode === ENTER) {
+          Ajax().get(`https://api.github.com/users/${value}`)
+            .then((result) => {
+              console.log(result)
+            })
+        } */
+
+        console.log('KeyCode:', keyCode)
+      }} />
   </div>
 )
 
