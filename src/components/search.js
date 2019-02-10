68aadb5ceb85dee110ddadaa8bd29f051689767b
jrquadros <quadrosjunioradits@gmail.com>
2019-02-10 20:05:17 -0300

    pupulando os dados do usuario

--- src/components/search.js
+++ src/components/search.js
@@ -1,42 +1,18 @@
 'use strict'
 
-import React from 'react'
-import Fetch from 'node-fetch'
-// import Ajax from '@fdaciuk/ajax'
+import React, { PropTypes } from 'react'
 
-const Search = () => (
+const Search = ({ handleSearch }) => (
   <div className='search'>
     <input
       type='search'
       placeholder='Digite o nome do usuário'
-      onKeyUp={(e) => {
-        const value = e.target.value
-        const keyCode = e.which || e.keyCode
-        const ENTER = 13
-
-        if (keyCode === ENTER) {
-          Fetch(`https://api.github.com/users/${value}`, {
-            method: 'get'
-          })
-            .then((response) => {
-              response.json()
-                .then((result) => {
-                  console.log(result)
-                  console.log(result.login)
-                })
-            })
-        }
-
-        /*  if (keyCode === ENTER) {
-          Ajax().get(`https://api.github.com/users/${value}`)
-            .then((result) => {
-              console.log(result)
-            })
-        } */
-
-        console.log('KeyCode:', keyCode)
-      }} />
+      onKeyUp={handleSearch} />
   </div>
 )
+
+Search.propTypes = {
+  handleSearch: PropTypes.func.isRequired
+}
 
 export default Search
